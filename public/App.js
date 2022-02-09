8d092012aa9aa5b668da3b0065695a6aa5c727b8
saurabhdeshp <deshpandesaurabh98@gmail.com>
2022-02-08 18:51:53 -0800

    added all the component; working properly

--- public/App.js
+++ public/App.js
@@ -1,26 +1,108 @@
 class ProductTable extends React.Component {
   render() {
-    return /*#__PURE__*/React.createElement("h3", null, " Product Table");
+    return /*#__PURE__*/React.createElement("table", {
+      className: "bordered-table"
+    }, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", null, "Product Name"), /*#__PURE__*/React.createElement("th", null, "Price $"), /*#__PURE__*/React.createElement("th", null, "Category"), /*#__PURE__*/React.createElement("th", null, "Image"))), /*#__PURE__*/React.createElement("tbody", null, this.props.products.map(item => /*#__PURE__*/React.createElement("tr", {
+      key: item.id
+    }, /*#__PURE__*/React.createElement("td", null, item.name), /*#__PURE__*/React.createElement("td", null, item.price), /*#__PURE__*/React.createElement("td", null, item.category), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement("a", {
+      href: item.url,
+      target: "_blank"
+    }, " View "))))));
   }
 
 }
 
 class ProductAdd extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      "name": "",
+      "price": "",
+      "category": "",
+      "url": ""
+    };
+  }
+
+  onAddHandler(event) {
+    event.preventDefault();
+    this.props.onAdd({ ...this.state
+    });
+  }
+
+  onChangeHandler(event) {
+    event.preventDefault();
+    const fieldName = event.target.getAttribute("name");
+    const fieldValue = event.target.value;
+    const newState = this.state;
+    newState[fieldName] = fieldValue;
+    this.setState(newState);
+  }
+
   render() {
-    return /*#__PURE__*/React.createElement("h3", null, " Product Add");
+    return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h3", null, " Add a new product to Inventory"), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement("form", {
+      onSubmit: event => this.onAddHandler(event)
+    }, /*#__PURE__*/React.createElement("table", {
+      className: "unbordered-table"
+    }, /*#__PURE__*/React.createElement("tbody", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", null, "Category"), /*#__PURE__*/React.createElement("th", null, "Price Per Unit")), /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", null, /*#__PURE__*/React.createElement("select", {
+      className: "dropdown",
+      name: "category",
+      id: "category",
+      onChange: this.onChangeHandler.bind(this)
+    }, /*#__PURE__*/React.createElement("option", {
+      value: ""
+    }, "---select category---"), /*#__PURE__*/React.createElement("option", {
+      value: "shirt"
+    }, "shirt"), /*#__PURE__*/React.createElement("option", {
+      value: "jeans"
+    }, "jeans"), /*#__PURE__*/React.createElement("option", {
+      value: "jacket"
+    }, "jacket"))), /*#__PURE__*/React.createElement("th", null, /*#__PURE__*/React.createElement("input", {
+      name: "price",
+      placeholder: "$",
+      onChange: this.onChangeHandler.bind(this)
+    }))), /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", null, "Product Name"), /*#__PURE__*/React.createElement("th", null, "Image URL")), /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", null, /*#__PURE__*/React.createElement("input", {
+      name: "name",
+      onChange: this.onChangeHandler.bind(this)
+    })), /*#__PURE__*/React.createElement("th", null, /*#__PURE__*/React.createElement("input", {
+      name: "url",
+      onChange: this.onChangeHandler.bind(this)
+    }))), /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", null, /*#__PURE__*/React.createElement("button", null, "Add Product")))))));
   }
 
 }
 
 class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+    this.addProduct = this.addProduct.bind(this);
+  }
+
+  addProduct(product) {
+    const products = this.state.products;
+    product.id = products.length + 1;
+    products.push(product);
+    this.setState({
+      products: products
+    });
+  }
+
   render() {
-    return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Product List"), /*#__PURE__*/React.createElement(ProductTable, null), /*#__PURE__*/React.createElement(ProductAdd, null));
+    return /*#__PURE__*/React.createElement("div", {
+      className: "productList"
+    }, /*#__PURE__*/React.createElement("h1", null, "Product List"), /*#__PURE__*/React.createElement(ProductTable, {
+      products: this.state.products
+    }), /*#__PURE__*/React.createElement(ProductAdd, {
+      onAdd: this.addProduct
+    }));
   }
 
 }
 
 const App = () => /*#__PURE__*/React.createElement("div", {
   className: "rootCont"
-}, /*#__PURE__*/React.createElement("h1", null, "Company Inventory"), /*#__PURE__*/React.createElement("h4", null, "Showing all available products"), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(ProductList, null));
+}, /*#__PURE__*/React.createElement("h2", null, "Company Inventory"), /*#__PURE__*/React.createElement("h3", null, "Showing all available products"), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(ProductList, null));
 
 ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById('root'));
--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,124 @@
 class ProductTable extends React.Component {
     render() {
-        return <h3> Product Table</h3>
+        return (
+            <table className="bordered-table">
+              <thead>
+                <tr>
+                  <th>Product Name</th>
+                  <th>Price $</th>
+                  <th>Category</th>
+                  <th>Image</th>
+                </tr>
+              </thead>
+              <tbody>
+                  {
+                      this.props.products.map((item) => (
+                        <tr key={item.id}>
+                            <td>{item.name}</td>
+                            <td>{item.price}</td>
+                            <td>{item.category}</td>
+                            <td><a href={item.url} target="_blank"> View </a></td>
+                        </tr>
+                      ))
+                  }
+              </tbody>
+            </table>
+          );
     }
 }
 
 class ProductAdd extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {"name": "","price": "","category" : "","url" : ""}
+    }
+
+    onAddHandler(event) {
+        event.preventDefault();
+        this.props.onAdd({...this.state});
+        
+    }
+
+    onChangeHandler(event) {
+        event.preventDefault()
+        const fieldName  = event.target.getAttribute("name");
+        const fieldValue = event.target.value;
+        const newState = this.state
+        newState[fieldName] = fieldValue
+        this.setState(newState);
+    }
+
     render() {
-        return <h3> Product Add</h3>
+        return (
+            <div>
+                <h3> Add a new product to Inventory</h3>
+                <hr></hr>
+                <form onSubmit={(event)=>this.onAddHandler(event)}>
+                <table className="unbordered-table">
+                    <tbody >
+                        <tr>
+                            <th>Category</th>
+                            <th>Price Per Unit</th>
+                        </tr>
+                        <tr>
+                            <th>
+                                <select className="dropdown" name="category" id="category" onChange={this.onChangeHandler.bind(this)}>
+                                    <option value="">---select category---</option>
+                                    <option value="shirt">shirt</option>
+                                    <option value="jeans">jeans</option>
+                                    <option value="jacket">jacket</option>
+                                </select>
+                            </th>
+                            <th>
+                                <input name="price" placeholder="$" onChange={this.onChangeHandler.bind(this)}></input>
+                            </th>
+                        </tr>
+                        <tr>
+                            <th>Product Name</th>
+                            <th>Image URL</th>
+                        </tr>
+                        <tr>
+                            <th>
+                                <input name="name" onChange={this.onChangeHandler.bind(this)}></input>
+                            </th>
+                            <th>
+                                <input name="url" onChange={this.onChangeHandler.bind(this)}></input>
+                            </th>
+                        </tr>
+                        <tr>
+                            <th><button>Add Product</button></th>
+                        </tr>
+                    </tbody>
+                </table>
+                </form>
+            </div>
+        )
     }
 }
 
 class ProductList extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+            products:[]
+        }
+        this.addProduct = this.addProduct.bind(this)
+    }
+
+     addProduct(product) {
+        const products = this.state.products;
+        product.id = products.length +1;
+        products.push(product)
+        this.setState({products: products})
+    }
+
     render() {
         return (
-            <div>
+            <div className="productList">
                 <h1>Product List</h1>
-                <ProductTable />
-                <ProductAdd />
+                <ProductTable products={this.state.products}/>
+                <ProductAdd onAdd={this.addProduct} />
             </div>
         )
 
@@ -25,8 +127,8 @@ class ProductList extends React.Component {
 
 const App = () =>(
     <div className="rootCont">
-        <h1>Company Inventory</h1>
-        <h4>Showing all available products</h4>
+        <h2>Company Inventory</h2>
+        <h3>Showing all available products</h3>
         <hr></hr>
         <ProductList />
     </div>
